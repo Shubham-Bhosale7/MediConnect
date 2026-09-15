1666a6ed07662e5f892a9c8aa816bc5f78c320e2
Shubham-Bhosale7 <shubhambhosale2299@gmail.com>
2026-09-15 18:49:54 +0530

    commit

--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -11,33 +11,21 @@ const NavBar = () => {
   const navigate = useNavigate();
 
   const logout = () => {
-    if (aToken) {
-      setAToken("");
-      localStorage.removeItem("aToken");
-    }
-
-    if (dToken) {
-      setDToken("");
-      localStorage.removeItem("dToken");
-    }
-
     navigate("/");
+    aToken && setAToken("");
+    aToken && localStorage.removeItem("aToken");
+    dToken && setDToken("");
+    dToken && localStorage.removeItem("dToken");
   };
 
   return (
     <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white">
       <div className="flex items-center gap-2 text-xs">
-        <img
-          className="w-36 sm:w-40 cursor-pointer"
-          src={assets.logo}
-          alt=""
-        />
-
+        <img className="w-36 sm:w-40 cursor-pointer" src={assets.logo} alt="" />
         <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">
-          {aToken ? "Admin" : dToken ? "Doctor" : ""}
+          {aToken ? "Admin" : "Doctor"}
         </p>
       </div>
-
       <button
         onClick={logout}
         className="bg-primary text-white text-sm px-10 py-2 rounded-full"
